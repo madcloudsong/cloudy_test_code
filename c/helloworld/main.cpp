808d10199dbf72bbb9ead73aafed170e2dcf0c51
madcloud <madcloudsong@qq.com>
2014-12-13 15:47:46 +0800

    c hello world

---
+++ c/helloworld/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: madcloud
+ *
+ * Created on 2014年12月13日, 下午3:40
+ */
+
+#include <cstdlib>
+#include <stdio.h>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+    char a[] = "cloudy";
+    printf("hellow word, %s", a);
+    return 0;
+}
+
